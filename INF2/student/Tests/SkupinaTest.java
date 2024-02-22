44cba971ea8d5ba08cc329d5bbaad873afeb5623
Ady38 <chobot.adam.2004@gmail.com>
2024-02-22 10:35:27 +0100

    testovaci projekt

---
+++ INF2/student/Tests/SkupinaTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class SkupinaTest {
+
+    @org.junit.jupiter.api.Test
+    void pridaj() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void vypis() {
+    }
+}
